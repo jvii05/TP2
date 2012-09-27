130de3b47ec6584891a668585bf069b0861fbc79
jvii05 <jvsc_05@hotmail.com>
2012-09-27 14:53:00 -0600

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TP2Javier::Application.config.secret_token = '711dcb3fbfb96ef4b5e01946f1a78bd8c348708709e84384a23f4b0453cb84e47d9e473b694c9758fb671ae1c50371d8d58710c66fb0a559925b0dc6982205c3'
